ac6da8b19b3ecf3a0b5c1982decfab4e499b055f
Outhman Moumou <tangerino2011@gmail.com>
2025-06-26 00:15:54 +0100

    fix(db): update MySQL password for database connection

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,8 +1,7 @@
 <?php
 // connect to database
-$conn = mysqli_connect('localhost', 'outhman790', 'test123', 'annonces_database');
+$conn = mysqli_connect('localhost', 'outhman790', 'outhman790..!!', 'annonces_database');
 // check connection *
 if (!$conn) {
     echo 'An error occured:' . mysqli_connect_error();
 }
-?>
